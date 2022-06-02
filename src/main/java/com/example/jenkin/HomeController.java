247eaa5f4051a34d46856d4cbabe2fa8b4329ce1
Osama Almadhoun <deosama77@gmail.com>
2022-06-02 13:19:19 +0300

    init

---
+++ src/main/java/com/example/jenkin/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.jenkin;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/hello")
+    public String getHello(){
+         return  "Hello world is done";
+    }
+}
