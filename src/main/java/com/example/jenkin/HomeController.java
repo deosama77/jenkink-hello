3ceacaa8a0117ac7476cc28190728f3a094569f6
Osama Almadhoun <deosama77@gmail.com>
2022-06-02 14:14:57 +0300

    update

--- src/main/java/com/example/jenkin/HomeController.java
+++ src/main/java/com/example/jenkin/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/hello")
     public String getHello(){
-         return  "Hello world is done";
+         return  "Hello world is done update";
     }
 }
